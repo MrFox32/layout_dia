1b5e37c3a7f1c2e4929dfb8d5f36d863d44c9b76
Zhenya Khodko <zhodko32@gmail.com>
2022-11-13 21:21:16 +0200

    Air v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const slider = document.getElementById('slider');
+const previousLink = document.getElementById('slider-previous-link');
+const nextLink = document.getElementById('slider-next-link');
+let offset = 0;
+
+function moveLeft() {
+  if (offset === 0) {
+    offset = 200;
+
+    slider.style.left = -offset + '%';
+
+    return;
+  }
+
+  if (offset === 200) {
+    offset -= 100;
+
+    slider.style.left = -offset + '%';
+
+    return;
+  }
+
+  offset -= 100;
+
+  slider.style.left = -offset + '%';
+}
+
+function moveRight() {
+  if (offset === 200) {
+    slider.style.left = 0;
+
+    offset = 0;
+
+    return;
+  }
+
+  if (offset === 0) {
+    offset += 100;
+
+    slider.style.left = -offset + '%';
+
+    return;
+  }
+
+  offset += 100;
+
+  slider.style.left = -offset + '%';
+}
+
+previousLink.addEventListener('click', moveLeft);
+
+nextLink.addEventListener('click', moveRight);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-menu') {
+    document.body.classList.add('page--overflow-hidden');
+  } else {
+    document.body.classList.remove('page--overflow-hidden');
+  }
+});
